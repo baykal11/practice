3817a9e317c07e703456ee4cb33139628c895bfb
Dolon Askarbekov <dolon_aa@mail.ru>
2019-11-19 19:35:44 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //
+
+        List<Integer> nums = Arrays.asList(14,463,234,4,357,45,235,46,3);
+
+        
+    }
+}
